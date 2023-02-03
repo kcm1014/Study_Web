638c54dcf808a9580be669fda47378b42823008b
kcm1014 <kcm1014@naver.com>
2023-02-03 15:04:02 +0900

    s

---
+++ javascript/Do It!/img/2023022301.js
@@ -0,0 +1,13 @@
+console.log(name);  // undefined
+//console.log(name1);   Cannot access 'name1' before initialization
+//console.log(name2);   Cannot access 'name2' before initialization
+var name = "sst";
+let name1 = "sst1";
+const name2 = "sst2";
+
+console.log(name1);
+console.log(name2);
+
+console.log(typeof(12));
+console.log(typeof name1);
+console.log(typeof false);
